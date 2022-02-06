26a5b32d75383926ea60495b2b818ad6e18fb27f
George <georgesucuzhanay100@gmail.com>
2022-02-06 02:17:24 -0500

    worked on task b and starter file for edit-array.cpp

---
+++ funcs.cpp
@@ -0,0 +1,9 @@
+#include "funcs.h"
+
+void print_interval(int L, int U){
+    // int myArr[] = {}
+    for(int i = L; i < U; i++) {
+        // std::cout << i << "\n";
+
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "funcs.h"
+
+void print_interval(int L, int U);
+
+int main()
+{
+    std::cout << "Testing";
+    std::cout << print_interval(2,3);
+    return 0;
+}
