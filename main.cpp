43b3a0af3f47ae20ee7c642fb68c816400bb6f62
George <georgesucuzhanay100@gmail.com>
2022-02-08 20:26:29 -0500

    changed comment on list of nums in task a - main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@ void print_interval(int L, int U);
 
 int main()
 {
-    std::cout << "List of nums between 1 through 10: \n";
+    std::cout << "List of nums between 1 inclusive through 11 exclusive: \n";
     print_interval(1,11);
     std::cout << "\n";
+    std::cout << "\n";
 
-    std::cout << "List of nums between 10 through 20: \n";
+    std::cout << "List of nums between 10 (inclusive) through 21 (exclusive): \n";
     print_interval(10,21);
     std::cout << "\n";
-    
-    std::cout << "List of nums between 20 through 30: \n";
+    std::cout << "\n";
+
+    std::cout << "List of nums between 20 (inclusive) through 31 (exclusive): \n";
     print_interval(20,31);
     std::cout << "\n";
     return 0;
